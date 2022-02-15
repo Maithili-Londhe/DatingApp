70d43e8fc87f11be202a21cf5ceb8a00a151d6e4
Maithili-Londhe <97285718+Maithili-Londhe@users.noreply.github.com>
2022-02-15 12:40:08 +0530

    End of section 10

--- client/src/app/_interceptor/jwt.interceptor.ts
+++ client/src/app/_interceptor/jwt.interceptor.ts
@@ -13,11 +13,11 @@ import { take } from 'rxjs/operators';
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-  constructor(private accountservice : AccountService) {}
+  constructor(private accountService : AccountService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let currentUser : User;
-    this.accountservice.currentUser$.pipe(take(1)).subscribe(user => currentUser = user);
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => currentUser = user);
     if (currentUser) {
       request = request.clone({
         setHeaders: {
